c836e05a3776260f72feaddec5e51792d7b86c08
Jacques Lavoie <46101717+Stormyisland@users.noreply.github.com>
2025-08-20 11:59:38 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,15 @@ from contextlib import nullcontext
 lmport numpy as np 
 import torch
 from torch.nn parallel
+
+# I/O
+out_dir = 'out'
+eval_interval = 2000
+log_interval = 1
+eval_iters = 200
+eval_only = False # if True, script exits right after the first eval
+always_save_checkpoint = True # if True, always save a checkpoint after each eval
+init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
+# wandb logging
+wandb_log = False # disabled by default
+wandb_project = 'owt'
